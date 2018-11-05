1f6165450201c52931f1a24623a3cba55b253f1a
DanAtlas <abishev295@gmail.com>
2018-11-05 21:04:09 +0600

    Updates

---
+++ precache-manifest.7231933c5a7d81bc6e4578e61f3b3088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e0946c6f05f4bae058b709f0f6b93ec",
+    "url": "/todo-react/static/media/trash.3e0946c6.svg"
+  },
+  {
+    "revision": "38f2ee9f9983b67546412038fbf1ea5a",
+    "url": "/todo-react/static/media/fireworks.38f2ee9f.svg"
+  },
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "4103c3fa47cdd22e36aa",
+    "url": "/todo-react/static/js/main.4103c3fa.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/todo-react/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "4103c3fa47cdd22e36aa",
+    "url": "/todo-react/static/css/main.c26840ef.chunk.css"
+  },
+  {
+    "revision": "b9ad8989b2a1e93393cc0fad025a5ce5",
+    "url": "/todo-react/index.html"
+  }
+];
